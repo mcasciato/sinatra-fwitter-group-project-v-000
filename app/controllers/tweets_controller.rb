fd517193a7deb85d5334e8f689e7a74c93cb7dd8
Jen <jenanton01@gmail.com>
2016-06-14 21:45:33 +0300

    Add files via upload

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,27 @@
 class TweetsController < ApplicationController
 
+<<<<<<< HEAD
+  get '/tweets' do
+    "hello world"
+    erb :'tweets/tweets'
+  end
+
+  post '/tweets' do
+    "hello world"
+  end
+  
+  get '/tweets/create_tweet' do
+    if logged_in?.empty?
+      redirect "/users/login"
+    else
+      erb :'tweets/create_tweet'
+    end
+  end
 end
+=======
+  get '/tweets/new' do
+    erb :'tweets/create_tweet'
+  end
+
+end
+>>>>>>> 98492d5eccd0078cc4f3572fe93844596fd67123
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,24 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/login' do
+    if logged_in?
+    redirect to '/tweets'
+    else
+    erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect "/tweets"
+      else
+        erb :'/users/login'
+      end
+  end
+
   post '/signup' do
     if params[:password] == ""
       redirect '/signup'
@@ -21,4 +39,10 @@ class UsersController < ApplicationController
       redirect to '/signup'
     end
   end
+
+  get '/logout' do
+
+  end
+
+
 end
