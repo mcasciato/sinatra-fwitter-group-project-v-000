fa621f2ec4a7b2d93e85a9cdacd60e22b8ed5d41
sensei100 <jenanton01@gmail.com>
2016-06-14 21:31:58 +0300

    login working

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,3 +1,19 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    "hello world"
+    erb :'tweets/tweets'
+  end
+
+  post '/tweets' do
+    "hello world"
+  end
+  
+  get '/tweets/create_tweet' do
+    if logged_in?.empty?
+      redirect "/users/login"
+    else
+      erb :'tweets/create_tweet'
+    end
+  end
 end
