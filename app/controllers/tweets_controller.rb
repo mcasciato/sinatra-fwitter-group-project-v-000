9fdfbb7333866271c70088a260621d5353fb3291
mcasciato <michael.casciato@icloud.com>
2016-06-14 17:18:29 -0400

    made sure signup and login tests passing

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,17 +1,17 @@
 class TweetsController < ApplicationController
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+# <<<<<<< HEAD
+# =======
+# <<<<<<< HEAD
   get '/tweets' do
-    "hello world"
+    "hello world" 
     erb :'tweets/tweets'
   end
 
   post '/tweets' do
     "hello world"
   end
-  
+
   get '/tweets/create_tweet' do
     if logged_in?.empty?
       redirect "/users/login"
@@ -19,15 +19,14 @@ class TweetsController < ApplicationController
       erb :'tweets/create_tweet'
     end
   end
-end
-=======
->>>>>>> 73185dec98942c2be5a5e29bcc88765a1e5677fd
+# =======
+# >>>>>>> 73185dec98942c2be5a5e29bcc88765a1e5677fd
   get '/tweets/new' do
     erb :'tweets/create_tweet'
   end
 
 end
-<<<<<<< HEAD
-=======
->>>>>>> 98492d5eccd0078cc4f3572fe93844596fd67123
->>>>>>> 73185dec98942c2be5a5e29bcc88765a1e5677fd
+# <<<<<<< HEAD
+# =======
+# >>>>>>> 98492d5eccd0078cc4f3572fe93844596fd67123
+# >>>>>>> 73185dec98942c2be5a5e29bcc88765a1e5677fd
