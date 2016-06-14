34ff2d26d28aeda7a9fe37a46ee354d164720d1e
mcasciato <michael.casciato@icloud.com>
2016-06-14 11:10:39 -0400

    passing signup tests, added helper methods in main app controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,24 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !session[:user_id]
+      erb :'/users/create_user'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    if params[:password] == ""
+      redirect '/signup'
+    end
+    user = User.new(params)
+    if user.username != "" && user.email != ""
+      user.save
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to '/signup'
+    end
+  end
 end
