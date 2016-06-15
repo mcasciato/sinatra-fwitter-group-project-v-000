867fb730b214ed71af2793435de1aad5e95394c1
mcasciato <michael.casciato@icloud.com>
2016-06-15 10:53:51 -0400

    some more tests passing

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,24 +1,42 @@
 class TweetsController < ApplicationController
 
   get '/tweets' do
-    "hello world"
-    erb :'tweets/tweets'
-  end
-
-  post '/tweets' do
-    "hello world"
-  end
-
-  get '/tweets/create_tweet' do
-    if logged_in?.empty?
-      redirect "/users/login"
+    # "hello world"
+    if !session[:user_id]
+      redirect to '/login'
     else
-      erb :'tweets/create_tweet'
+      @user = User.find(session[:user_id])
+      erb :'/tweets/tweets'
     end
   end
 
   get '/tweets/new' do
-    erb :'tweets/create_tweet'
+    if !session[:user_id]
+      redirect to '/login'
+    else
+      erb :'/tweets/create_tweet'
+    end  end
+
+  post '/tweets' do
+    # "hello world"
+    if !params[:content].empty?
+      tweet = Tweet.create(content: params[:content])
+      tweet.user_id = session[:user_id]
+      tweet.save
+      redirect to '/tweets'
+    else
+      redirect to '/tweets/new'
+    end
   end
+
+  # get '/tweets/create_tweet' do
+  #   if logged_in?.empty?
+  #     redirect "/users/login"
+  #   else
+  #     erb :'tweets/create_tweet'
+  #   end
+  # end
+
+
 
 end
