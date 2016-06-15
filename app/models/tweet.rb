8a2b6be18543b07f528d3bd241ef57d9bdaaaa6d
mcasciato <michael.casciato@icloud.com>
2016-06-15 17:35:27 -0400

    updates

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
-  extend Slugifiable::ClassMethods
-  include Slugifiable::InstanceMethods
+  # extend Slugifiable::ClassMethods
+  # include Slugifiable::InstanceMethods
 end
